eac142f505d2856dd42ab926f9b4c3dd0707e086
Ahuoiza <oizabaiye@gmail.com>
2019-03-06 17:26:44 -0500

    create login and logout states

--- src/App.js
+++ src/App.js
@@ -1,47 +1,101 @@
-import React, { Component } from 'react';
-import './App.css';
-import Header from './components/Header';
-import Form from './components/Form';
-import List from './components/List';
-import Memo from './components/Memo.js';
+import React, { Component } from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import firebase from 'firebase';
+import Header from "./components/Header";
+import Form from "./components/Form";
+import List from "./components/List";
+import Memo from "./components/Memo.js";
+import Login from './components/Login';
 
+//sets google as the authentication provider thru firebase
+const provider = new firebase.auth.GoogleAuthProvider();
+const auth = firebase.auth();
+
+//we changed database read and wright rules in firebase 
 
 class App extends Component {
   constructor() {
-    super()
+    super();
     this.state = {
-      //setState to empty string
-      userName: 'test user'
-    }
+      userName: null
+    };
+  }
+  //set state by default to null (upon home page load, no username)
+
+  //function that gets the user in upon clicking the 'login' button (conditionally rendered)
+  login = () => {
+    auth.signInWithPopup(provider).then((result) => {
+      const user = result.user;
+      this.setState({
+        userName: user
+      })
+    })
+  }
+  
+//function that logs out upon clicking the 'logout' button
+  logout = () => {
+    auth.signOut().then(() => {
+      this.setState({
+        userName: null
+      })
+    })
   }
 
-
+  //upon page load, if a user is logged in, persist the login
+  componentDidMount(){
+    auth.onAuthStateChanged(user => {
+      if (user) {
+        this.setState({
+          user: user
+        })
+      }
+    })
+  }
 
   render() {
     return (
       <div className="App">
         <Header />
         <main>
-          <div className="intro">
-            <h1>DOOMSDAYYYYYYY</h1>
-            {/* description */}
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur libero voluptatum vero praesentium molestiae assumenda accusantium sequi delectus reiciendis odit eveniet ex inventore omnis explicabo culpa itaque laudantium, enim vel.</p>
+          <div className="wrapper">
+            <div className="intro">
+              <h1>DOOMSDAYYYYYYY</h1>
+              {/* description */}
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Consequatur libero voluptatum vero praesentium molestiae
+                assumenda accusantium sequi delectus reiciendis odit eveniet ex
+                inventore omnis explicabo culpa itaque laudantium, enim vel.
+              </p>
+            </div>
+            {/* node is prop we pass to form */}
+            <section className="form clearfix">
+              <div>
+                <Form
+                  node="list"
+                  label="add to list"
+                  userName={this.state.userName}
+                />
+                <List />
+              </div>
+              <div>
+                <Form
+                  node="memo"
+                  label="add memo"
+                  userName={this.state.userName}
+                />
+                <Memo />
+              </div>
+            </section>
           </div>
-          {/* node is prop we pass to form */}
-          <section className="form clearfix">
-            <div>
-              <Form node='list' label='add to list' userName={this.state.userName} />
-              <List />
-            </div>
-            <div>
-              <Form node='memo' label='add memo' userName={this.state.userName} />
-              <Memo />
-            </div>
-          </section>
+          <Login user={this.state.userName} login={this.login} logout={this.logout}/>
+
         </main>
       </div>
     );
   }
 }
+
 
 export default App;
---
+++ src/components/Login.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route, Link
+} from 'react-router-dom';
+
+//we use this.props.user (not state) because we're passing the user property from App.js
+
+//login logout functions were written in App.js and will be passed here as props
+
+class Login extends Component {
+  render(){
+    return(
+      <Router>
+        <div>
+          <h1>title</h1>
+          <p>description of amazing app</p>
+          {this.props.user ? <button onClick={this.props.logout}>Log Out</button> : <button onClick={this.props.login}>Log In</button>}
+        </div>
+      </Router>
+    )
+  } //the button onclick functions are in App. So we're passing those functions in by props. The username is put in App (not login) bcuz we'll be accessing it in both components
+}
+
+export default Login;
