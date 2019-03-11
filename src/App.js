ffd9918ac2211348ebde7c0d2f3f62d258cee51d
Alexander <alexander.shin87@gmail.com>
2019-03-11 14:22:14 -0400

    added map api

--- src/App.js
+++ src/App.js
@@ -7,15 +7,20 @@ import Bunker from './components/Bunker.js';
 
 import Footer from './components/Footer.js';
 import Header from './components/Header.js';
+import Map from './components/Map.js';
 
-import "./components/doom.jpg"
+// import './components/doom.jpg';
+import alexBunker from './assets/alexBlue.jpg';
+import glenBunker from './assets/glenBlue.jpg';
+import oizaBunker from './assets/oizaBlue.jpg';
+import zoeBunker from './assets/zoeBlue.jpg';
 
 
 //sets google as the authentication provider thru firebase
 const provider = new firebase.auth.GoogleAuthProvider();
 const auth = firebase.auth();
 
-//we changed database read and write rules in firebase 
+//we changed database read and write rules in firebase
 
 //change any place where bunker1 is used
 //creating 3 more bunkers
@@ -29,48 +34,47 @@ class App extends Component {
     super();
     this.state = {
       userName: null,
-      bunker: '',
+      bunker: ''
     };
   }
   //set state by default to null (upon home page load, no username)
 
   //function that gets the user in upon clicking the 'login' button (conditionally rendered)
   login = () => {
-    auth.signInWithPopup(provider).then((result) => {
+    auth.signInWithPopup(provider).then(result => {
       const user = result.user;
       this.setState({
-        userName: user,
-      })
-    })
-  }
+        userName: user
+      });
+    });
+  };
 
   //function that logs out upon clicking the 'logout' button
   logout = () => {
     auth.signOut().then(() => {
       this.setState({
         userName: null
-      })
-    })
-  }
+      });
+    });
+  };
 
   guest = () => {
     const guestName = {
       displayName: 'Guest'
-    }
+    };
     this.setState({
       userName: guestName
-    })
-  }
+    });
+  };
 
-  setBunker = (event) => {
+  setBunker = event => {
     //Search for specific bunker from bunkerList and set as current Bunker
     //2. Make firebase call get info on specific bunker? Set as list
     const bunkerChoice = event.target.value;
     this.setState({
       bunker: bunkerChoice
-    })
-  }
-
+    });
+  };
 
   //upon page load, if a user is logged in, persist the login
   componentDidMount() {
@@ -79,63 +83,110 @@ class App extends Component {
       if (user) {
         this.setState({
           userName: user
-        })
+        });
       }
-    })
+    });
   }
 
   componentDidUpdate() {
     console.log(this.state.bunker);
   }
 
-
   render() {
     return (
       <div className='App'>
-        {this.state.userName ? <Header logout={this.logout} user={this.state.userName.displayName} /> : null}
-          <div className='wrapper'>
-        <main>
-
+        {this.state.userName ? (
+          <Header
+            logout={this.logout}
+            user={this.state.userName.displayName}
+          />
+        ) : null}
+        <div className='wrapper'>
+          <main>
             <div>
               {/* used ternary operator to check if userName is truthy then show Bunker component otherwise show Login component */}
-              {this.state.userName ?
+              {this.state.userName ? (
                 <div>
-                  <Intro/>
-                  <form className="bunkerNames" action="" onChange={this.setBunker}>
-                      <input type="radio" name="bunkerChoice" id="alex" value="alex" />
-                      <label htmlFor='alex'>Alex's Bunker</label>
-               
-                      <input type="radio" name="bunkerChoice" id="glen" value="glen" />
-                      <label htmlFor="glen">Glen's Bunker</label>
-                 
-
-                
-                      <input type="radio" name="bunkerChoice" id="oiza" value="oiza" />
-                      <label htmlFor="oiza">Oiza's Bunker</label>
-                    
-
-                    
-                      <input type="radio" name="bunkerChoice" id="zoe" value="zoe" /> <label htmlFor="zoe">Zoe's Bunker</label>
-                
+                  <Intro />
+                  <form
+                    className='bunkerNames clearfix'
+                    action=''
+                    onChange={this.setBunker}
+                  >
+                    <input
+                      type='radio'
+                      name='bunkerChoice'
+                      id='alex'
+                      value='alex'
+                    />
+                    <label htmlFor='alex' className='alexBunkerLabel'>
+                      <p>Alex's Bunker</p>
+                      <img
+                        src={alexBunker}
+                        alt='image of a house on a mountain'
+                        className='bunkerImage'
+                      />
+                    </label>
+                    <input
+                      type='radio'
+                      name='bunkerChoice'
+                      id='glen'
+                      value='glen'
+                    />
+                    <label htmlFor='glen'>
+                      <p>Glen's Bunker</p>
+                      <img
+                        src={glenBunker}
+                        alt='image of a city landscape'
+                        className='bunkerImage'
+                      />
+                    </label>
+                    <input
+                      type='radio'
+                      name='bunkerChoice'
+                      id='oiza'
+                      value='oiza'
+                    />
+                    <label htmlFor='oiza'>
+                      <p>Oiza's Bunker</p>
+                      <img
+                        src={oizaBunker}
+                        alt='image of sand dune with several people shown standing on the dune'
+                        className='bunkerImage'
+                      />
+                    </label>
+                    <input
+                      type='radio'
+                      name='bunkerChoice'
+                      id='zoe'
+                      value='zoe'
+                    />{' '}
+                    <label htmlFor='zoe'>
+                      <p>Zoe's Bunker</p>
+                      <img src={zoeBunker} alt='image of a country road' className='bunkerImage' />
+                    </label>
                   </form>
-                  <Bunker userName={this.state.userName} choice={this.state.bunker} />
+
+                  <Bunker
+                    userName={this.state.userName}
+                    choice={this.state.bunker}
+                  />
                 </div>
-                :
+              ) : (
                 <Login
                   userName={this.state.userName}
                   login={this.login}
                   guest={this.guest}
                 />
-              }
-
+              )}
             </div>
-        </main>
-        <Footer />
+          <Map choice={this.state.bunker}/>
+          </main>
+          <Footer />
+        </div>
       </div>
-          </div>
     );
   }
 }
-
 
 export default App;
---
+++ src/components/Map.js
@@ -0,0 +1,94 @@
+import React, { Component } from 'react';
+
+class Map extends Component {
+  getMap = () => {
+    if (this.props.choice === 'alex') {
+      return (
+        <div>
+          <h2>Alex's Bunker</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Quaerat dolorum accusantium harum adipisci voluptates
+            consequuntur. Laboriosam quia consequuntur nesciunt fuga!
+          </p>
+          <a href='https://www.google.ca/maps/dir//483+Queen+St+W,+Toronto,+ON+M5V+2A9/@43.6483351,-79.4000419,17z/data=!4m8!4m7!1m0!1m5!1m1!1s0x882b34dc87fa67bd:0xe05376a9e49647e1!2m2!1d-79.3978242!2d43.6482738' target='_blank'>
+            <img
+              className='alex'
+              src='https://www.mapquestapi.com/staticmap/v5/map?key=T0v8EvAMROc0iDiMAoFFnblYHAdslHMH&locations=483 Queen Street West, Toronto, ON M5V 2A9&defaultMarker=marker-red-lg'
+              alt=''
+            />
+          </a>
+        </div>
+      );
+    } else if (this.props.choice === 'glen') {
+      return (
+        <div>
+          <h2>Glen's Bunker</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Quaerat dolorum accusantium harum adipisci voluptates
+            consequuntur. Laboriosam quia consequuntur nesciunt fuga!
+          </p>
+          <a
+            href='https://www.google.ca/maps/dir//955+Lake+Shore+Blvd+W,+Toronto,+ON+M6K+3B9/@43.6305144,-79.4491849,13z/data=!4m8!4m7!1m0!1m5!1m1!1s0x882b350e29f4d8c1:0x8176e955dd91f7b3!2m2!1d-79.4141654!2d43.6304576'
+            target='_blank'
+          >
+            <img
+              src='https://www.mapquestapi.com/staticmap/v5/map?key=T0v8EvAMROc0iDiMAoFFnblYHAdslHMH&locations=955 Lake Shore Blvd W, Toronto, ON M6K 3B9&defaultMarker=marker-red-lg'
+              alt=''
+            />
+          </a>
+        </div>
+      );
+    } else if (this.props.choice === 'oiza') {
+      return (
+        <div>
+          <h2>Oiza's Bunker</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Quaerat dolorum accusantium harum adipisci voluptates
+            consequuntur. Laboriosam quia consequuntur nesciunt fuga!
+          </p>
+          <a
+            href='https://www.google.ca/maps/dir//Chinguetti,+Mauritania/@20.459705,-12.3724616,15z/data=!4m8!4m7!1m0!1m5!1m1!1s0xe84120b09cf2bf9:0xb0f6ba637c97e0a2!2m2!1d-12.3664341!2d20.4615121'
+            target='_blank'
+          >
+            <img
+              src='https://www.mapquestapi.com/staticmap/v5/map?key=T0v8EvAMROc0iDiMAoFFnblYHAdslHMH&locations=Route Chinguetti, Chinguetti 1234, Mauritania&defaultMarker=marker-red-lg'
+              alt=''
+            />
+          </a>
+        </div>
+      );
+    } else if (this.props.choice === 'zoe') {
+      return (
+        <div>
+          <h2>Zoe's Bunker</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Quaerat dolorum accusantium harum adipisci voluptates
+            consequuntur. Laboriosam quia consequuntur nesciunt fuga!
+          </p>
+          <a
+            href='https://www.google.ca/maps/dir//Seventeen+Seventy+QLD+4677,+Australia/@-24.1612988,151.850188,13z/data=!3m1!4b1!4m8!4m7!1m0!1m5!1m1!1s0x6be99008cb7c339b:0x400eef17f20c990!2m2!1d151.8852075!2d-24.1613811'
+            target='_blank'
+          >
+            <img
+              src='https://www.mapquestapi.com/staticmap/v5/map?key=T0v8EvAMROc0iDiMAoFFnblYHAdslHMH&locations=Seventeen Seventy Queensland 4677, Australia&defaultMarker=marker-red-lg'
+              alt=''
+            />
+          </a>
+        </div>
+      );
+    } else 
+    return (
+      <h2>Select a Bunker!</h2>
+    )
+  };
+
+  render() {
+    return <section>{this.getMap()}</section>;
+  }
+}
+
+export default Map;
